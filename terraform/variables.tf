df13e45eeeb78e2ee525bd5eb0e2efedfda04434
Nevio Marzo <nevio.marzo@edu.tbz.ch>
2026-05-22 16:46:17 +0200

    Initialised terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project_name" {
+  description = "Projektname"
+  type        = string
+  default     = "m300"
+}
